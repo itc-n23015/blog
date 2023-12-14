bd79cd0160fdec7573ba23010d8b11130b5bee54
itc-n23015 <n23015@std.it-college.ac.jp>
2023-12-14 05:26:51 +0000

    2.4 p52 finished
    jsconfig.jsonにBaseのパスを設定

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Header from '../components/header.js'
-import Hero from '../components/hero.js'
-import Footer from '../components/footer.js'
+import Header from 'components/header.js'
+import Hero from 'components/hero.js'
+import Footer from 'components/footer.js'
 
 export default function Home () {
   return (
