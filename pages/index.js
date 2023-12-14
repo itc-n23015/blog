ba396abcca9996716028e6052e91dca43fef79aa
itc-n23015 <n23015@std.it-college.ac.jp>
2023-12-14 06:57:37 +0000

    2.6 p56
    LayoutをAPPコンポーネントに入れた

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
-import Layout from 'components/layout'
 import Hero from 'components/hero.js'
 
 export default function Home () {
-  return (
-    <Layout>
-      <Hero />
-    </Layout>
-  )
+  return <Hero />
 }
