d06d4a33360d9b4eb37f2edbf4b28382feec9691
itc-n23015 <n23015@std.it-college.ac.jp>
2024-01-09 04:56:33 +0000

    6.4 p191

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero.js'
 const Home = (title, subtitle) => {
   return (
     <Container>
+      <Meta />
       <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
     </Container>
   )
